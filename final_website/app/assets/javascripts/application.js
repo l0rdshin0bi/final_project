a352218d3146b9a4205cbdc0828abfb67d3af6ee
Irvin R Archer <lord_peene@yahoo.com>
2017-09-13 01:44:26 -0700

    commit

--- final_website/app/assets/javascripts/application.js
+++ final_website/app/assets/javascripts/application.js
@@ -13,3 +13,16 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  var audioSection = $('section#song');
+  $('a.play_song').click(function() {
+
+  var audio = $('<audio>', {
+      controls : 'controls'
+  });
+
+  var url = $(this).attr('href');
+  $('<source>').attr('src', url).appendTo(audio);
+  audioSection.html(audio);
+  return false;       });
+}); 
--- final_website/config/routes.rb
+++ final_website/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+#this is my index pages
+  root 'pages#home'
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
